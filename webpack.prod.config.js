9796294ad2164e5b124903c3ed99443f42b0f03a
Han Li <han.li@finra.org>
2016-07-18 14:52:50 -0400

    some prod webpack plugin

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,4 +1,12 @@
 var devConf = require('./webpack.config.js');
+var webpack = require("webpack");
+
 devConf.devtool = 'eval';
+var prodPlugins = [
+	new webpack.optimize.DedupePlugin(),
+	new webpack.optimize.OccurenceOrderPlugin(),
+	new webpack.optimize.UglifyJsPlugin({mangle: false, sourcemap: false}),
+];
+devConf.plugins.push(...prodPlugins);
 
 module.exports = devConf;
